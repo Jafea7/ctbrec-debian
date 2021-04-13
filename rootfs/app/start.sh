60628c0e149e91fa56a712475315c441b2c8cd19
Jafea7 <73450040+Jafea7@users.noreply.github.com>
2021-04-13 13:50:23 +1000

    Update 4.1.2

--- rootfs/app/start.sh
+++ rootfs/app/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
 
 cd ${HOME}
-java -Xmx192m -cp ctbrec-server-4.1.1-final.jar -Dctbrec.config.dir=/app/config -Dctbrec.config=server.json ctbrec.recorder.server.HttpServer
+java -Xmx192m -cp ctbrec-server-4.1.2-final.jar -Dctbrec.config.dir=/app/config -Dctbrec.config=server.json ctbrec.recorder.server.HttpServer
