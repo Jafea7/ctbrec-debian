ed1ad1bc19636e7cc23519368e8cff86c6aad2ad
Jafea7 <73450040+Jafea7@users.noreply.github.com>
2021-07-19 12:51:52 +1000

    CTBRec 4.4.3 server update

--- rootfs/app/start.sh
+++ rootfs/app/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
 
 cd ${HOME}
-java -Xmx256m -cp ctbrec-server-4.4.2-final.jar -Dfile.encoding=utf-8 -Dctbrec.config.dir=/app/config -Dctbrec.config=server.json ctbrec.recorder.server.HttpServer
+java -Xmx256m -cp ctbrec-server-4.4.3-final.jar -Dfile.encoding=utf-8 -Dctbrec.config.dir=/app/config -Dctbrec.config=server.json ctbrec.recorder.server.HttpServer
