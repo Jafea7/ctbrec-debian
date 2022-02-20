2c6774a3a6bc9e294cb027929badc4c07dd7412d
Jafea7 <73450040+Jafea7@users.noreply.github.com>
2022-02-20 22:16:55 +1100

    Update CTBRec 4.7.2

--- rootfs/app/start.sh
+++ rootfs/app/start.sh
@@ -4,7 +4,7 @@ cd ${HOME}
 # If logback.xml/server.log exists then redirect output to server.log on the host
 # requires the server.log to be mapped by Docker.
 if [ -f /app/config/logback.xml ] && [ -f /app/config/server.log ]; then
-  java -Xmx256m -cp ctbrec-server-4.7.1-final.jar -Dfile.encoding=utf-8 -Dctbrec.config.dir=/app/config -Dctbrec.config=server.json ctbrec.recorder.server.HttpServer >> /app/config/server.log
+  java -Xmx256m -cp ctbrec-server-4.7.2-final.jar -Dfile.encoding=utf-8 -Dctbrec.config.dir=/app/config -Dctbrec.config=server.json ctbrec.recorder.server.HttpServer >> /app/config/server.log
 else
-  java -Xmx256m -cp ctbrec-server-4.7.1-final.jar -Dfile.encoding=utf-8 -Dctbrec.config.dir=/app/config -Dctbrec.config=server.json ctbrec.recorder.server.HttpServer
+  java -Xmx256m -cp ctbrec-server-4.7.2-final.jar -Dfile.encoding=utf-8 -Dctbrec.config.dir=/app/config -Dctbrec.config=server.json ctbrec.recorder.server.HttpServer
 fi
