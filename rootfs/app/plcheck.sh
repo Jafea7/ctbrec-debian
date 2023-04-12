9497d9b02f824651010f4ab4b230adafedd53592
Jafea7 <73450040+Jafea7@users.noreply.github.com>
2023-04-12 22:26:52 +1000

    Fix parameter bug

--- rootfs/app/plcheck.sh
+++ rootfs/app/plcheck.sh
@@ -2,9 +2,4 @@
 # Server non-single file use only.
 # plcheck.sh ${absolutePath}
 
-path="${1%/}/playlist.m3u8"
-if [[ -f "${path}" ]]; then
-  if [[ $(tail -1 "${path}" | xargs) != "#EXT-X-ENDLIST" ]]; then
-    echo "#EXT-X-ENDLIST" >> "${path}"
-  fi
-fi
+[[ -d "${1}" ]] && [[ -f "${1%/}/playlist.m3u8" ]] && echo "#EXT-X-ENDLIST" >> "${1%/}/playlist.m3u8" || exit 0
