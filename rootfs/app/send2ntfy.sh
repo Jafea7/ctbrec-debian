8068700a4a21d5b75144f493d74714829f9ad0e2
jafea7 <jafea7@gmail.com>
2024-03-04 18:36:07 +1100

    Create send2ntfy.sh

---
+++ rootfs/app/send2ntfy.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# send2ntfy.sh ${absolutePath} [${modelDisplayName} ${localDateTime(yyyyMMdd-HHmmss)} ...]
+
+file="$1"
+sheet="${file%.*}.jpg"
+length=`/app/ffmpeg/ffmpeg -i "$file" 2>&1 | grep "Duration" | cut -d ' ' -f 4 | sed s/,// | sed 's@\..*@@g'`
+shift
+
+if [ -z "$1" ]; then
+  content="Contact sheet"
+else
+  content="$1"
+  shift
+  for i in $@
+  do
+    content=`echo "${content} - ${i}"`
+  done
+fi
+content=`echo "${content}: ${length}"`
+
+curl -F 'payload_json={"username": "CTBRec", "content":"'"$content"'"}' -F "file1=@$sheet" $DISCORDHOOK
